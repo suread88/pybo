165520657fea1d3b0981b7d85590fbce8257d6ab
suread <suread@daum.net>
2025-01-25 18:16:08 +0900

    settings 변경

---
+++ config/settings/prod.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = ['49.247.7.104']
--- mysite.cmd
+++ mysite.cmd
@@ -1,2 +1,4 @@
+@echo off
 cd D:\myDjango\projects\mysite
+set DJANGO_SETTINGS_MODULE=config.settings.local
 D:\myPY\venv\mysite\Scripts\activate
