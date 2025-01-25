51c9b64f0e3be727a416ddb47770ec06062f8db4
suread <suread@daum.net>
2025-01-25 23:23:51 +0900

    로깅 설정

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from ..models import Question
 from django.db.models import Q, Count
 
+#import logging
+#logger = logging.getLogger(__name__)
 
 def index(request):
+#   logger.info('INFO 레벨로 출력')
     page = request.GET.get('page','1')
     kw = request.GET.get('kw','');
     so = request.GET.get('so', 'recent');
